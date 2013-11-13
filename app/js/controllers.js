fe0d0d177f05bc7af8deb255bf62100b8c0b1605
Tracy Rojas <tcrojas@gmail.com>
2013-11-12 23:43:55 -0800

    Parse the ID out of the URL to build the event details URL

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,5 +6,9 @@ var app = angular.module('myApp.controllers', []);
 
 
 app.controller('EventListCtrl', ['$scope', 'SennaEvents', function ($scope, SennaEvents) {
-	$scope.events = SennaEvents.query();
+	$scope.events = SennaEvents.query(function (response) {
+		angular.forEach(response, function (item) {
+			item.id = item.url.substring(item.url.lastIndexOf('/')+1,item.url.lastIndexOf('.'));
+		});
+	});
 }]);
